91026313552b23e60ce67e238540ef2bd7949b85
Victor Aremu <me@victorare.mu>
2024-07-21 01:54:11 +0100

    feat: v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import { useEffect } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
 import useEscape from "./hooks/useEscape";
 
 import "./App.css";
 
 function App() {
   useEscape();
-  useEffect(() => {
-    invoke("init_spotlight_window");
-  }, []);
 
   return (
     <div className="container">
       <h2>Tauri MacOS Spotlight App</h2>
+
       <p style={{ margin: 0 }}>
-        Press <kbd>Cmd</kbd>+<kbd>k</kbd> to toggle the spotlight window,
-        <br />
-        or press <kbd>Esc</kbd> to hide window.
+        Press <kbd>Cmd</kbd>+<kbd>k</kbd> to toggle the spotlight window, or
+        press <kbd>Esc</kbd> to hide window.
       </p>
+
       <form style={{ margin: "10px 0" }}>
         <input type="text" name="text" placeholder="Search..." />
       </form>
+
       <small className="well">
         This <mark>NSWindow</mark> was converted to <mark>NSPanel</mark> at
         runtime.
       </small>
-      <br />
     </div>
   );
 }
--- src/hooks/useEscape.tsx
+++ src/hooks/useEscape.tsx
@@ -1,16 +1,18 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/core";
 import { useEffect } from "react";
 
 const useEscape = () => {
   const handleEscape = (event: KeyboardEvent) => {
     if (event.key === "Escape") {
       event.preventDefault();
-      invoke("hide_spotlight");
+
+      invoke("hide");
     }
   };
 
   useEffect(() => {
     window.addEventListener("keydown", handleEscape);
+
     return () => window.removeEventListener("keydown", handleEscape);
   }, []);
 };
